5fba0582747fb34d507546d48dc07f8c48e82d65
Sgenio <sgenio@Sgenios-MacBook-Pro.local>
2018-07-23 17:24:42 +0100

    The begining of a big journey

---
+++ app.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" 
+        href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" 
+        integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" 
+        crossorigin="anonymous">
+
+    <link rel="stylesheet" href="css/app.css">
+
+    <title>LeanSread dashboard</title>
+</head>
+<body>
+    
+
+    <div class="screen container-fluid">
+
+        <div class="topHeader">
+            <div class="logo">
+                <label for="leanSpread">leanSpread</label>
+            </div><!-- End logo -->
+
+            <div class="listMenu">
+
+                <ul>
+                    <!-- If logged -->
+                    <li class="loggedUserPicture">
+                        <img src="img/user/loggedUserPitureTemplate.png" alt="User logged">
+                    </li>
+
+                    <li class="loggedUserName">Rolf Domingos</li>
+
+                    <li class="loggedUserCategory">Administrador</li>
+
+                    <li class="loggedMenu">Menu</li>
+
+                    <!-- End if logged -->
+
+                </ul> 
+
+            </div><!-- End list Menu -->
+
+            <div class="clearFix"></div>
+
+            <div class="container">
+                <eside class="sideMenu">
+
+
+                    <!-- In this layout the footer is part of eside menu -->
+                    <footer>
+
+                    </footer><!-- End footer -->
+
+                </eside><!-- End container -->
+
+                <div class="content">
+
+                </div><!-- End Content -->
+
+            </div><!-- End container -->
+
+        </div><!-- End toHeader -->
+
+
+        
+
+         
+
+    </div><!-- End Screen -->
+
+
+
+</body>
+</html>
