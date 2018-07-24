a851cf2782898079d26c5f7d076057cbd3021d21
sgenio <sousadgaspar@gmail.com>
2018-07-24 07:10:53 +0100

    add eside and content container

--- app.php
+++ app.php
@@ -31,9 +31,9 @@
                         <img src="img/user/loggedUserPitureTemplate.png" alt="User logged">
                     </li>
 
-                    <li class="loggedUserName">Rolf Domingos</li>
-
-                    <li class="loggedUserCategory">Administrador</li>
+                    <li class="loggedUserName">Rolf Domingos 
+                        <span class="loggedUserCategory">Administrador</span>
+                    </li>
 
                     <li class="loggedMenu">Menu</li>
 
@@ -42,29 +42,40 @@
                 </ul> 
 
             </div><!-- End list Menu -->
-
+            
             <div class="clearFix"></div>
 
-            <div class="container">
-                <eside class="sideMenu">
-
-
-                    <!-- In this layout the footer is part of eside menu -->
-                    <footer>
-
-                    </footer><!-- End footer -->
-
-                </eside><!-- End container -->
-
-                <div class="content">
-
-                </div><!-- End Content -->
-
-            </div><!-- End container -->
 
         </div><!-- End toHeader -->
 
+        <div class="container">
+            <eside class="sideMenu">
 
+                <p> something here! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe itaque odio accusantium omnis dolore autem possimus praesentium, porro, nostrum ipsam voluptatibus inventore accusamus alias velit impedit ea rerum laudantium sint?</p>
+                <!-- In this layout the footer is part of eside menu -->
+                <footer>
+
+                </footer><!-- End footer -->
+
+            </eside><!-- End container -->
+
+            <div class="content">
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex veniam, odit architecto ducimus ut quae. Ex, alias illum veritatis, eos mollitia vel illo animi necessitatibus commodi, optio molestias reiciendis earum.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil quaerat doloremque minus non nesciunt nulla eos aperiam maiores. Quo quas neque mollitia fuga illo, animi deserunt id non libero sit?
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur, cupiditate inventore repellat, harum soluta asperiores id libero veniam eveniet aut doloribus maiores! Repudiandae, iste! Dignissimos eligendi mollitia voluptates dolor architecto.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit perferendis, eius odit a praesentium molestiae dolor hic. Explicabo ducimus qui et iusto excepturi enim, corrupti iure eveniet dolore? Sunt, praesentium!
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti ad fuga cumque, eveniet laboriosam unde molestias iusto nostrum expedita nihil quo illum quam, accusamus officiis impedit aliquid. Dolorum, ex consequuntur.
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga aliquid illo iste. Natus illo consequuntur delectus. Vitae obcaecati, nisi ad inventore, consectetur porro, possimus quisquam nesciunt quis qui quasi corporis.</p>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex veniam, odit architecto ducimus ut quae. Ex, alias illum veritatis, eos mollitia vel illo animi necessitatibus commodi, optio molestias reiciendis earum.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil quaerat doloremque minus non nesciunt nulla eos aperiam maiores. Quo quas neque mollitia fuga illo, animi deserunt id non libero sit?
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur, cupiditate inventore repellat, harum soluta asperiores id libero veniam eveniet aut doloribus maiores! Repudiandae, iste! Dignissimos eligendi mollitia voluptates dolor architecto.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit perferendis, eius odit a praesentium molestiae dolor hic. Explicabo ducimus qui et iusto excepturi enim, corrupti iure eveniet dolore? Sunt, praesentium!
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti ad fuga cumque, eveniet laboriosam unde molestias iusto nostrum expedita nihil quo illum quam, accusamus officiis impedit aliquid. Dolorum, ex consequuntur.
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga aliquid illo iste. Natus illo consequuntur delectus. Vitae obcaecati, nisi ad inventore, consectetur porro, possimus quisquam nesciunt quis qui quasi corporis.</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id corrupti amet ea qui architecto. Suscipit incidunt debitis veniam nulla eos! Perspiciatis, laudantium excepturi? Perspiciatis nostrum ducimus quisquam ipsam reprehenderit illo.</p>
+            </div><!-- End Content -->
+
+        </div><!-- End container -->
         
 
          
